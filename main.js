430093687d367aa054bc53093df8184365142f2d
yoni-jo <yoni9258@naver.com>
2020-11-20 16:01:45 +0900

    스크롤 함수 변경
    변수 스크롤 함수에 넣고
    header변수 셀렉터 변수명 추가 하여 메인과 이벤트페이지 구분

--- main.js
+++ main.js
@@ -1,51 +1,59 @@
 'use strict';
-const sectionsList = new Array();
-let section = document.querySelectorAll('section')
-
-const n = 50;
-section.forEach((sections) => {
-
-
-    sectionsList.push(sections)
-
-})
 
 // 스크롤시 section안에 백그라운드 이미지위치값이 변경
-
-
 function scroll() {
+
+
+// section 배열로 하나씩 저장
+
+
+    const sectionsList = new Array();
+    let section = document.querySelectorAll('section')
+    const n = 50;
+    
+    
+    section.forEach((sections) => {
+
+
+        sectionsList.push(sections)
+    
+    })
+    
+
+
     let num = window.scrollY;
 
-    let first = 50 - num -30;
-    let second = first+num-20 ;
-    let third = second+(num/10)-40
-    const header = document.querySelector("header")
+    let first = 50 - num - 30;
+    let second = first + num - 20;
+    let third = second + (num / 10) - 40;
+    const header = document.querySelector("header.scroll")
 
-    //sectionsList.forEach((sectionsLists) => {
 
-        if (window.scrollY >= header.offsetHeight) {
-            
-            sectionsList[0].style.backgroundPosition = `50% ${first}px`;
-            
-                
-             sectionsList[1].style.backgroundPosition = `50% ${second}px`;
-           
-            
-              sectionsList[2].style.backgroundPosition = `50% ${third}px`;
 
-         
+    // 스크롤이 헤더높이값과 같거나 클경우 
+    if (window.scrollY >= header.offsetHeight) {
 
-        }else if(window.scrollY< header.offsetHeight){
-            sectionsList[0].style.backgroundPosition = `50% 50px`;
-            
-                
-            sectionsList[1].style.backgroundPosition = `50% 50px`;
-          
-           
-             sectionsList[2].style.backgroundPosition = `50% 50px`;
+        sectionsList[0].style.backgroundPosition = `50% ${first}px`;
 
-        }
-       ;
- 
+
+        sectionsList[1].style.backgroundPosition = `50% ${second}px`;
+
+
+        sectionsList[2].style.backgroundPosition = `50% ${third}px`;
+
+
+    //스크롤시 헤더높이값보다 작을경우 초기값 유지하게 설정
+    } else if (window.scrollY < header.offsetHeight) {
+        sectionsList[0].style.backgroundPosition = `50% 50px`;
+
+
+        sectionsList[1].style.backgroundPosition = `50% 50px`;
+
+
+        sectionsList[2].style.backgroundPosition = `50% 50px`;
+
+    };
+
 }
 window.addEventListener('scroll', scroll)
+
